6d919d59db734220602db042d680e77bf7776cb8
Nathan Ortbals <nathan.ortbals@gmail.com>
2019-04-11 13:44:52 -0500

    Fixed DFS endpoint

--- routes/dfs.js
+++ routes/dfs.js
@@ -4,18 +4,8 @@ const dfsQuery = require('../middleware/dfsQuery');
 const asyncWrapper = require('../middleware/asyncWrapper');
 
 router.get('/', validation.getDfs, asyncWrapper(async function(req, res) {
-
   const data = await dfsQuery(req.query.position, req.query.id);
-
-  var positions = [];
-  data.forEach(function(entry) {
-    if(entry.player) {
-      if(!positions.includes(entry.player.position)) {
-        positions.push(entry.player.position);
-      }
-    }
-  });
-  res.json(positions);
+  res.json(data);
 }));
 
 module.exports = router;

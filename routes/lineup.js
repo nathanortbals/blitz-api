4159cac42ed8c8962031961e90f1e8bfa1c305bc
Nathan Ortbals <nathan.ortbals@gmail.com>
2019-05-12 11:07:56 -0500

    Second attempt at fixing bug

--- routes/lineup.js
+++ routes/lineup.js
@@ -18,31 +18,19 @@ router.get('/', validation.getLineup, asyncWrapper(async function(req, res) {
         getDfsQuery('D', 'D', req)
     ]);
 
-    res.json ({
-        qb: results[0][0],
-        rb1: results[1][0],
-        rb2: results[2][0],
-        wr1: results[3][0],
-        wr2: results[4][0],
-        wr3: results[5][0],
-        te: results[6][0],
-        f: results[7][0],
-        d: results[8][0],
-    });
+    let result = {
+      qb: results[0][0],
+      rb1: results[1][0],
+      rb2: results[2][0],
+      wr1: results[3][0],
+      wr2: results[4][0],
+      wr3: results[5][0],
+      te: results[6][0],
+      f: results[7][0],
+      d: results[8][0],
+    };
 
-    // let results = await getLineup(
-    //     req.query.QB,
-    //     req.query.RB1,
-    //     req.query.RB2,
-    //     req.query.WR1,
-    //     req.query.WR2,
-    //     req.query.WR3,
-    //     req.query.TE,
-    //     req.query.F,
-    //     req.query.D
-    // );
-    //
-    // res.json(results);
+    res.json(result);
 }));
 
 async function getDfsQuery(position, lineupPosition, req) {
